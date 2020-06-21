141a44aeee2349041a0c31545c74f5f878f8fb5c
Valentin Grégoire <valentin.gregoire91@gmail.com>
2020-06-21 22:02:42 +0200

    update 2100

--- src/router/views/evasion/fantasy/2100/2100.js
+++ src/router/views/evasion/fantasy/2100/2100.js
@@ -50,36 +50,36 @@ export default {
             ]
         },
         {
-            title: "Le village gris (2)",
-            date: "26/06/2020",
+            title: "Le lac rose (1)",
+            date: "21/06/2020",
             content: [
                 {
                     tag: 'p',
-                    value: 'Pierre ouvre un œil. Pas deux. Il n’en possède qu’un à ouvrir. Ça pourrait être triste s’il n’était pas né comme ça. Mais il est né comme ça. Comme tous les enfants du village gris, il lui manque quelque chose. Entre eux, ils s’appellent les « pas finis ». Ils ne savent pas si on les appelle autrement, ailleurs. Pierre y réfléchit parfois. Il ne sait pas si c’est le cas des autres.'
+                    value: 'Le soleil est déjà haut dans le ciel blanchâtre et le lac rose se pare de reflets dorés. Sur le bord du lac, tout près de l’eau scintillante aux teintes inhabituelles, Goldwynn se prélasse. Goldwynn aime la chaleur, les rayons bienfaisants. Elle s’étire au maximum pour offrir plus de surface au soleil, elle sent ses corolles qui se dilatent, pour boire la lumière et l’énergie. Ses feuilles frémissent et se réorientent légèrement.'
                 },
                 {
                     tag: 'p',
-                    value: 'Lui-même s’en veut de s’inquiéter d’un ailleurs. Il n’y a pas d’ailleurs pour ceux du village gris. Personne n’en sort. On n’y entre que tout petit, et toujours par le même chemin. On y entre par le Tube. Pierre n’a pas trop envie de penser au Tube, ça lui donne le cafard.'
+                    value: 'Goldwynn est bien. Goldwynn somnole. Elle digère. Elle a bien mangé, énormément mangé même. En général, sa posture un peu à l’écart du groupe, plus près de l’eau, lui vaut de n’avoir que des restes. Mais pas cette fois. Cette fois, Goldwynn s’est taillée la part du lion. Un petit miracle, qu’elle ne comprend pas bien. Elle comprendra peut-être plus tard. Pour l’instant, elle digère.'
                 },
                 {
                     tag: 'p',
-                    value: 'Quand il a le cafard, il est lent et il ne rapporte pas beaucoup à manger. Seuls les plus vifs peuvent attraper les geckos gris, et seuls les geckos gris se mangent au village. Les geckos gris et les champignons. Mais à la longue, les champignons rendent fous.'
+                    value: 'C’est le début du processus. L’énergie de la viande se détache lentement de la chair au fur et à mesure que l’acide dans le ventre de Goldwynn la dissout. L’énergie est précieuse. Seule l’énergie permet à Goldwynn de se tenir là et de revoir le soleil jour après jour. En même temps, Goldwynn reçoit l’Histoire. L’Histoire de sa proie. Des images, des sons, des odeurs… toute une vie en accéléré, en quelques semaines de digestion.'
                 },
                 {
                     tag: 'p',
-                    value: 'Tout est gris dans le village gris. Pierre se demande s’il s’est toujours appelé comme ça. Ou si on l’appelle différemment, ailleurs. Ou s’il a eu un autre nom dans le temps. Il est comme ça, Pierre, il se demande toujours des choses. Après, il finit par avoir mal à la tête. Et Fleur se moque de lui, gentiment.'
+                    value: 'Normalement, ce ne sont que des bribes, des flashs de quelques secondes, qui ne forment pas un tout cohérent. L’Histoire est la saveur de la viande. Mais cette fois, c’est différent. Goldwynn a mangé toute la proie, au lieu de recevoir un petit morceau, une petite part comme chacune dans la colonie. C’est comme si la proie avait choisi Goldwynn. Comme si la proie avait eu pitié d’elle, toute seule sur le bord du lac. Mais c’est impossible. Les proies montrent généralement de l’agressivité et puis de la peur, et chaque proie fait son lot de victime dans la colonie.'
                 },
                 {
                     tag: 'p',
-                    value: 'Fleur a deux yeux, mais elle n’a pas d’oreille. Juste un trou étrange d’un côté et rien de l’autre. Elle aime rire Fleur. Elle raconte bien les histoires. Parfois, elle raconte qu’elle a perdu ses oreilles dans le Tube. Qu’elle est restée coincée en venant et que, en se débattant, elle s’est arraché les oreilles.'
+                    value: 'Goldwynn n’est pas un humain. Goldwynn est une fleur mémorielle. Une fleur carnivore. Les humains disent « cannibale », mais ce n’est pas correct : Goldwynn ne mange pas ses sœurs fleurs. Seulement les humains. Les humains qui pleurent et qui écrasent et qui finissent par revenir, attirés par les tissus colorés des souvenirs jalousement gardés par les fleurs mémorielles.'
                 },
                 {
                     tag: 'p',
-                    value: 'Les autres savent que c’est faux, bien sûr. Pierre a vu Fleur toute nue, et il ne lui manque rien d’autre. Or les enfants intacts ne sont pas envoyés au village gris. N’empêche, c’est une belle histoire.'
+                    value: 'Goldwynn habite le lac rose, un lieu maudit. Les humains évitent de plus en plus cet endroit féérique dont personne ne revient. La colonie des fleurs mémorielles dépérit. Elles sont bien moins nombreuses qu’autrefois. Il ne reste plus que les plus fortes, regroupées à l’avant, et Goldwynn, toute jeune, près du lac. Elles sont condamnées. Mais il n’est pas dans la nature des fleurs de s’inquiéter de cela.'
                 },
                 {
                     tag: 'p',
-                    value: 'Pierre esquisse un sourire. Même sans être à ses côtés, Fleur a de nouveau réussi. Elle a chassé le cafard. Il se lève. Il ramasse son lance-pierre. Il croit entendre le bruissement des geckos gros, en contrebas. Il enfile sa tunique grise. Il ressemble aux murs, au sol, au ciel du village gris. Il est prêt.'
+                    value: 'Goldwynn profite, Goldwynn savoure, Goldwynn digère, au soleil.'
                 },
             ]
         },
